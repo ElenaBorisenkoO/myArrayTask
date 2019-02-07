c4f2c92c73aca8213ae3cb4594c5460a7d3ef3d3
Lena <elena.borisenko1991@gmail.com>
2019-02-07 15:20:49 +0000

    1.create method pop 2.create method push

--- src/main.js
+++ src/main.js
@@ -16,12 +16,40 @@ var OwnArray = /** @class */ (function () {
         }
     }
     OwnArray.prototype.forEach = function (callback, thisArg) {
+        if (thisArg === void 0) { thisArg = this; }
         for (var i = 0; i < this.length; i++) {
-            callback(thisArg, this.elementData[i], i, this);
+            callback(this.elementData[i], i, this);
         }
+    };
+    OwnArray.prototype.push = function () {
+        var data = [];
+        for (var _i = 0; _i < arguments.length; _i++) {
+            data[_i] = arguments[_i];
+        }
+        for (var i = 0; i < data.length; i++) {
+            var currentLength = this.length;
+            this.elementData[currentLength] = data[i];
+            this.length += 1;
+        }
+        return this.length;
+    };
+    OwnArray.prototype.pop = function () {
+        var resultElement = this.elementData[this.elementData.length - 1];
+        if (this.elementData.length !== 0) {
+            var newElements = new OwnArray();
+            for (var i = 0; i < this.elementData.length - 1; i++) {
+                newElements.elementData[i] = this.elementData[i];
+            }
+            this.elementData.length -= 1;
+        }
+        return resultElement;
     };
     return OwnArray;
 }());
 var x = new OwnArray(1, 2, 3);
 //x.forEach((thisArg:any,data:any,index:number,src:any):void => console.log(thisArg), x);
-x.forEach(function (thisArg, data, index, src) { return console.log(data); });
+//x.forEach((elem) => console.log(elem));
+// console.log(x.push(8,7,7,"geg"));
+// console.log(x);
+console.log(x.pop());
+console.log(x);
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-class OwnArray {
+class OwnArray<T>{
     length: number;
     elementData: any[] = [];
     constructor(...data: any) {
@@ -11,17 +11,38 @@ class OwnArray {
             }
         }
     }
-forEach(callback: Function, thisArg?: any) {  //что делать с thisArg, если он не задан. Нужен ли он вообще?Какой тип?
+    forEach(callback: (data: any, index: number, src: any) => any, thisArg: any = this): void {  //что делать с thisArg, если он не задан. Нужен ли он вообще?Какой тип?
         for (let i = 0; i < this.length; i++) {
-            callback(thisArg, this.elementData[i], i, this);
+            callback(this.elementData[i], i, this);
         }
+    }
+    push(...data: any): number {
+        for (let i = 0; i < data.length; i++) {
+            const currentLength = this.length;
+            this.elementData[currentLength] = data[i];
+            this.length += 1;
+        }
+        return this.length;
+    }
+    pop(): any {
+        const resultElement = this.elementData[this.elementData.length - 1];
 
+        if (this.elementData.length !== 0) {
+            let newElements = new OwnArray();
+            for (let i = 0; i < this.elementData.length - 1; i++) {
+                newElements.elementData[i] = this.elementData[i];
+            }
+            this.elementData.length -= 1;
+        }
+        return resultElement;
     }
 }
 
 
-
-
 let x = new OwnArray(1, 2, 3);
 //x.forEach((thisArg:any,data:any,index:number,src:any):void => console.log(thisArg), x);
-x.forEach((thisArg: any, data: any, index: number, src: any): void => console.log(data));
+//x.forEach((elem) => console.log(elem));
+// console.log(x.push(8,7,7,"geg"));
+// console.log(x);
+console.log(x.pop());
+console.log(x);
